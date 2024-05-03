830927bc10550a74a67f395f726f350e4fa487aa
Alex C <calex781@gmail.com>
2024-05-03 09:27:57 -0400

    Install nodemon.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3005;
 
-app.get("/", (req, res) => {
+app.get("/api/recipes", (req, res) => {
+  const foodQuery = req.query.food;
   res.send("Hello");
 });
 
