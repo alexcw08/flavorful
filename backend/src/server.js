259b6bafa97c39c96c5bc8c1ab9f947a640c4856
Alex C <calex781@gmail.com>
2024-05-03 15:29:30 -0400

    Install express validator.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 3005;
 
-app.get("/api/recipes", (req, res) => {
-  const foodQuery = req.query.food;
-  res.send("Hello");
-});
+const { query, validationResult } = require("express-validator");
+
+app.get(
+  "/api/recipes",
+  query("food").trim().notEmpty().escape(),
+  (req, res) => {
+    const validateErrors = validationResult(req);
+    if (validateErrors.isEmpty()) {
+      // Valid request
+      const foodQuery = req.query.food;
+      res.send("Hello");
+    } else {
+      res.json({ message: "Invalid query." });
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
