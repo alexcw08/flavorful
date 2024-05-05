b59f63a9ca019a15172e59744e143e0021cdbf5b
Alex C <calex781@gmail.com>
2024-05-05 15:21:29 -0400

    Add test cases for fetching recipes.

--- backend/test/api.test.js
+++ backend/test/api.test.js
@@ -12,4 +12,18 @@ describe("API Endpoints testing", () => {
     expect(response.body.recipes).to.be.an("array");
     expect(response.body.recipes.length).to.equal(5);
   });
+
+  it("returns an object containing an array of results of recipes", async () => {
+    const response = await request.get("/api/recipes/?food=cheeseburgers");
+    expect(response.status).to.equal(200);
+    expect(response.body).to.be.an("object");
+    expect(response.body.results).to.be.an("array");
+  });
+
+  it("returns an error when sending an empty query", async () => {
+    const response = await request.get("/api/recipes/?food=");
+    expect(response.status).to.equal(400);
+    expect(response.body).to.be.an("object");
+    expect(response.body.errors[0].type).to.equal("field");
+  });
 });
