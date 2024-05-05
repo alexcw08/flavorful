733b34a4c04ac69a567dbc334174c2d1907614a2
Alex C <calex781@gmail.com>
2024-05-05 15:30:10 -0400

    Add test cases for retrieving information for specific recipe id.

--- backend/test/api.test.js
+++ backend/test/api.test.js
@@ -5,6 +5,7 @@ import app from "../src/app.js";
 const request = supertest(app);
 
 describe("API Endpoints testing", () => {
+  // fetching random recipes
   it("returns 5 random recipes", async () => {
     const response = await request.get("/api/recipes-random");
     expect(response.status).to.equal(200);
@@ -13,6 +14,7 @@ describe("API Endpoints testing", () => {
     expect(response.body.recipes.length).to.equal(5);
   });
 
+  // fetching a search for recipes
   it("returns an object containing an array of results of recipes", async () => {
     const response = await request.get("/api/recipes/?food=cheeseburgers");
     expect(response.status).to.equal(200);
@@ -25,5 +27,22 @@ describe("API Endpoints testing", () => {
     expect(response.status).to.equal(400);
     expect(response.body).to.be.an("object");
     expect(response.body.errors[0].type).to.equal("field");
+  });
+
+  // fetching a specific recipe based on id
+
+  it("returns an object for valid recipe id", async () => {
+    const response = await request.get("/api/recipes/123");
+    expect(response.status).to.equal(200);
+    expect(response.body).to.be.an("object");
+    expect(response.body).to.have.property("id");
+  });
+
+  it("returns an error object for an invalid recipe id", async () => {
+    const response = await request.get("/api/recipes/9999999999");
+    expect(response.status).to.equal(404);
+    expect(response.body).to.be.an("object");
+    expect(response.body).to.have.property("error");
+    expect(response.body.error).to.equal("Not Found");
   });
 });
