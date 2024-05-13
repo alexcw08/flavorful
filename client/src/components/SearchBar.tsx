78254037b754a70fd11c80ad704a56a088bdeac9
Alex C <calex781@gmail.com>
2024-05-13 16:48:48 -0400

    Create component for searching.

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,21 @@
+export default function SearchBar() {
+  return (
+    <div className=" w-full m-auto py-4">
+      <label className="input input-bordered w-2/4 m-auto flex items-center gap-2">
+        <input type="text" className="grow" placeholder="Search for recipes" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="w-4 h-4 opacity-70 cursor-pointer"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </label>
+    </div>
+  );
+}
