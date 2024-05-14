7d64821021fe02e2a065c4df87eb90669ee0e9c1
Alex C <calex781@gmail.com>
2024-05-14 09:51:42 -0400

    Install and mount cors middleware.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import recipeRoutes from "./routes/recipeRoutes.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 recipeRoutes(app);
 
 export default app;
