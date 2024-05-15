519a1ecf5f39c26e041153738dabd6bdad6fabb7
Alex C <calex781@gmail.com>
2024-05-15 18:44:47 -0400

    Make components responsive to larger screen sizes.

--- client/src/components/FeaturedRecipes.tsx
+++ client/src/components/FeaturedRecipes.tsx
@@ -4,7 +4,7 @@ import { RecipeCard } from "./RecipeCard.js";
 
 export default function FeaturedRecipes() {
   return (
-    <section className=" md:px-5">
+    <section className="md:px-5 ">
       <h1 className="text-xl pl-4 py-2 font-semibold   md:text-3xl ">
         Featured Recipes
       </h1>
@@ -12,7 +12,7 @@ export default function FeaturedRecipes() {
         Discover a curated collection of delicious recipes, handpicked just for
         you.
       </h2>
-      <div className="flex flex-col items-center py-6 gap-y-6  ">
+      <div className=" flex flex-col items-center md:grid md:grid-cols-2 lg:grid-cols-5 py-6 gap-y-6 md:gap-x-8  lg:w-full lg:gap-x-12 ">
         {defaultData.recipes.map((recipe) => (
           <RecipeCard recipe={recipe} />
         ))}
--- client/src/components/RecipeCard.tsx
+++ client/src/components/RecipeCard.tsx
@@ -2,23 +2,41 @@ export const RecipeCard = ({ recipe }) => {
   let recipeImageURL = null;
 
   return (
-    <div className="card rounded-none lg:rounded-box">
-      {/* image */}
-      <figure className="">
+    <div className="flex flex-col pb-2     rounded-xl">
+      <figure className="  w-full rounded-xl">
         {recipe.image ? (
-          <img className=" md:object-cover " src={recipe.image} alt="" />
+          <img className="w-full  rounded-t-lg" src={recipe.image} alt="" />
         ) : (
-          <img className=" md:object-cover " src="/no-image.jpeg" alt="" />
+          <img
+            // className="absolute inset-0 w-full h-full object-cover object-center"
+            className="w-full rounded-t-lg"
+            src="/no-image.jpeg"
+            alt=""
+          />
         )}
       </figure>
-      <div className="card-body w-full items-center py-1 ">
-        {/* name of recipe */}
-        <h2 className="text-center h-14 md:h-20 w-40 md:text-xl  overflow-hidden">
+      <div className="flex flex-col flex-1  w-full px-4">
+        <h1 className=" text-xl w-full h-16 pt-2 text-wrap font-semibold truncate">
           {recipe.title}
-        </h2>
-        <div className="flex w-full flex-col items-center gap-y-2 mb-2 ">
-          <div className="card-actions ">
-            <button className="btn px-4">View Recipe</button>
+        </h1>
+        <div className="flex  w-full flex-1">
+          <div className="w-3/5 h-24 flex flex-col md:flex-row md:flex-wrap gap-x-4 gap-y-2   py-2  md:px-2 md:w-9/12">
+            {recipe.vegetarian ? (
+              <div className="badge badge-neutral py-4">Vegetarian </div>
+            ) : null}
+            {recipe.vegan ? (
+              <div className="badge badge-neutral py-4">Vegan </div>
+            ) : (
+              <div className="badge badge-neutral py-4">Vegan </div>
+            )}
+            {recipe.glutenFree ? (
+              <div className="badge badge-neutral py-4">Gluten-free </div>
+            ) : null}
+          </div>
+          <div className=" flex flex-1 justify-center items-center ">
+            <button className="btn px-6 md:px-4 lg:px-6 text-lg font-light md:text-base">
+              View
+            </button>
           </div>
         </div>
       </div>
