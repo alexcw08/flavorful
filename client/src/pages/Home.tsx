361143111adacd1094f04466469b3a44e84bc41d
Alex C <calex781@gmail.com>
2024-05-15 12:50:26 -0400

    Add some text to home page for user.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,6 +6,16 @@ function Home() {
   return (
     <>
       <Header />
+      <div className="px-2 py-4 text-center">
+        <h1 className="text-2xl font-bold mb-2">
+          Explore over 5,000 diverse recipes.
+        </h1>
+        <h2 className="text-xl font-semibold">
+          Complete with detailed nutrition analysis, handy cooking tips, and
+          related recipe suggestions to inspire your culinary creativity.
+        </h2>
+      </div>
+
       <SearchBar />
       <RecipeOfDay />
       <RandomRecipes />
