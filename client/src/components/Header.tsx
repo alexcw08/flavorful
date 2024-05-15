7390b5903a9d7567cf9b0d59ad216a7e4c538bc9
Alex C <calex781@gmail.com>
2024-05-15 12:39:35 -0400

    Redo header styling.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,54 +1,45 @@
 export const Header = () => {
   return (
-    <header className="navbar px-0 md:px-4  justify-between">
-      <h1 className="text-lg md:text-xl lg:text-2xl ml-4 md:ml-0">Flavorful</h1>
-
-      <ul className="w-1/2  flex justify-center gap-8 md:gap-0 md:menu md:menu-horizontal ">
-        <li>
-          <a className="md:text-xl " href="">
-            Home
-          </a>
-        </li>
-        <li className="disabled">
-          <a className="md:text-xl " href="">
-            Recipes
-          </a>
-        </li>
-        <li className="disabled">
-          <a className="md:text-xl " href="">
-            Nutrition
-          </a>
-        </li>
-      </ul>
-      <div className="flex-none gap-2">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar mr-2 "
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt=""
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+    <header className="navbar justify-between  bg-slate-700">
+      <div className="navbar-start ">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
               />
-            </div>
+            </svg>
           </div>
           <ul
             tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a className="justify-between">Profile</a>
+              <a>Home</a>
             </li>
             <li>
-              <a>Settings</a>
+              <a>Recipes</a>
             </li>
             <li>
-              <a>Logout</a>
+              <a>Nutrition</a>
             </li>
           </ul>
         </div>
+      </div>
+      <div className="navbar-center">
+        <a className="btn btn-ghost text-xl">Flavorful</a>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-ghost">Sign out</button>
       </div>
     </header>
   );
