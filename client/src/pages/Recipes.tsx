7f0eee93b75468626185d12f7fe4c3aedc22f6e0
Alex C <calex781@gmail.com>
2024-06-10 18:59:23 -0400

    Add label when no results are found for query.

--- client/src/pages/Recipes.tsx
+++ client/src/pages/Recipes.tsx
@@ -9,12 +9,15 @@ export default function Recipes() {
     <>
       <Header />
       <SearchBar setSearchResults={setSearchResults} />
+
       <div className=" flex flex-col items-center md:grid md:grid-cols-2 lg:grid-cols-5 py-6 gap-y-6 md:gap-x-8  lg:w-full lg:gap-x-12 ">
-        {searchResults.results.length != 0
-          ? searchResults.results.map((recipe) => {
-              return <RecipeCard recipe={recipe} />;
-            })
-          : null}
+        {searchResults.results.length != 0 ? (
+          searchResults.results.map((recipe) => {
+            return <RecipeCard recipe={recipe} />;
+          })
+        ) : (
+          <h1 className="font-bold ml-4 text-xl">No results found.</h1>
+        )}
       </div>
     </>
   );
